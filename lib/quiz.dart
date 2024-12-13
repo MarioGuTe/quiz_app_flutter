2a59a8314e244d49964fdf5ef4651bfa3ef5c0b2
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-13 01:56:19 -0300

    update switch screen logic to use ternary operator instead of initState

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,18 +10,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
+  // Widget? activeScreen;
 
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
+
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(
       () {
-        activeScreen = const QuestionsScreen();
+        activeScreen = 'questions-screen';
       },
     );
   }
@@ -41,7 +43,10 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: Center(child: activeScreen),
+          child: Center(
+              child: activeScreen == 'start-screen'
+                  ? StartScreen(switchScreen)
+                  : const QuestionsScreen()),
         ),
       ),
     );
