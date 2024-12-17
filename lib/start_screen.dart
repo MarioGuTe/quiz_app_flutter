5350bc236120ba0545ee6b975d8cfef899671dfc
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-17 02:32:35 -0300

    add  structure and style

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,38 +7,40 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 280,
-          color: const Color.fromARGB(150, 255, 255, 255),
-        ),
-        const SizedBox(height: 80),
-        const Text(
-          'Learn Flutter The Fun Way!',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        const SizedBox(height: 20),
-        OutlinedButton.icon(
-          onPressed: startQuiz,
-          style: OutlinedButton.styleFrom(
-            side: const BorderSide(color: Color.fromARGB(41, 255, 255, 255)),
-            foregroundColor: Colors.white,
-            padding: const EdgeInsets.symmetric(
-              horizontal: 24,
-              vertical: 12,
-            ),
-            textStyle: const TextStyle(fontSize: 16),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8), // Rounded corners
-            ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 280,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
-          icon: const Icon(Icons.arrow_right_alt),
-          label: const Text('Start Quiz'),
-        )
-      ],
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter The Fun Way!',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              side: const BorderSide(color: Color.fromARGB(41, 255, 255, 255)),
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 24,
+                vertical: 12,
+              ),
+              textStyle: const TextStyle(fontSize: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8), // Rounded corners
+              ),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
     );
   }
 }
