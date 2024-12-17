26ae162cebd1b8cff04ae77df7b942d0c56ea4bb
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-17 02:30:11 -0300

    update to use AnserButton with iterable logic

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/answer_button.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -7,9 +9,32 @@ class QuestionsScreen extends StatefulWidget {
   State<QuestionsScreen> createState() => _QuestionsScreenState();
 }
 
+void submitAnswer() {
+  // TODO: Implement submitting the answer and checking if it's correct
+}
+
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('This is the Questions Screen');
+    final currentQuestion = questions[
+        0]; // Replace 0 with the index of the question you want to display.
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers
+              .map((item) => AnswerButton(item, submitAnswer)),
+        ],
+      ),
+    );
   }
 }
