4d89b72e655c1a74137835d3918d41a8d313d4ea
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-18 01:07:39 -0300

    add logic to shuffle questions

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -35,8 +35,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.answers
-                .map((item) => AnswerButton(item, submitAnswer)),
+            ...currentQuestion
+                .getShuffledAnswers()
+                .map((answer) => AnswerButton(answer, submitAnswer)),
           ],
         ),
       ),
