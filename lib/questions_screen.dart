7f847d1b98c41bcd883d8780c72ddad01ff16a86
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-18 00:54:22 -0300

    update style

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -21,19 +21,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers
-              .map((item) => AnswerButton(item, submitAnswer)),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers
+                .map((item) => AnswerButton(item, submitAnswer)),
+          ],
+        ),
       ),
     );
   }
